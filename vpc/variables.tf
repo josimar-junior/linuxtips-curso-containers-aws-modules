bce8a3cbaa29639db7b40bf2311cd9e31b500587
Josimar Junior <josimarjunior@Josimars-MacBook-Pro.local>
2026-02-11 07:51:19 -0300

    vpc module

---
+++ vpc/variables.tf
@@ -0,0 +1,31 @@
+variable "project_name" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "cidr" {
+  type = string
+}
+
+variable "availability_zone" {
+  type = list(string)
+}
+
+variable "private_subnets" {
+  type = list(map({
+    name              = string,
+    cidr              = string
+    availability_zone = string
+  }))
+}
+
+variable "public_subnets" {
+  type = list(map({
+    name              = string,
+    cidr              = string
+    availability_zone = string
+  }))
+}
