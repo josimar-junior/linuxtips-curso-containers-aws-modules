868c0a2d692a8c9d57fae93470f18910fdb130c1
Josimar Junior <josimarjunior@Josimars-MacBook-Pro.local>
2026-02-20 22:43:15 -0300

    fix public subnets

--- vpc/public_subnets.tf
+++ vpc/public_subnets.tf
@@ -37,5 +37,5 @@ resource "aws_route_table_association" "public" {
   count = length(var.public_subnets)
 
   subnet_id      = aws_subnet.public[count.index].id
-  route_table_id = aws_route_table.public_internet_access[count.index].id
+  route_table_id = aws_route_table.public_internet_access.id
 }
